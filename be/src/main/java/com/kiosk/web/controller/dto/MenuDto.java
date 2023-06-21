92640ea31f9cd047a493084f017a6f6401d56c7f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-06-21 17:34:07 +0900

    fix #5: Json에서 isBest -> best로 나오는 문제해결
    문제원인은 Jackson 라이브러리에서 getter/is와 같은 메소드를 가지고 직렬화/역직렬화를 수행하는데 isBest와 같은 메소드에서 json 데이터를 만드는 과정에서 best로 제목을 정하기 때문입니다.
    
    @JsonProperty 애노테이션을 사용하여 제목을 지정하여 문제를 해결했습니다.

--- be/src/main/java/com/kiosk/web/controller/dto/MenuDto.java
+++ be/src/main/java/com/kiosk/web/controller/dto/MenuDto.java
@@ -1,23 +1,31 @@
 package com.kiosk.web.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kiosk.domain.entity.Category;
 import com.kiosk.domain.entity.Product;
 import java.util.Objects;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class MenuDto {
 
     private Long menuId;            // 상품 아이디
-    private final String name;        // 이름
-    private final Long price;         // 가격
-    private final String imgUrl;       // 이미지 저장 경로
-    private final boolean isBest;     // 인기 상품
-    private final boolean hasHot;     // 핫 가능 여부
-    private final boolean hasIce;     // 아이스 가능 여부
-    private final boolean hasLarge;   // 라지 사이즈 가능 여부
-    private final boolean hasSmall;   // 스몰 사이즈 가능 여부
-    private final Long categoryId;    // 카테고리 아이디
+    private String name;        // 이름
+    private Long price;         // 가격
+    private String imgUrl;       // 이미지 저장 경로
+    @JsonProperty("isBest")
+    private boolean isBest;     // 인기 상품
+    private boolean hasHot;     // 핫 가능 여부
+    private boolean hasIce;     // 아이스 가능 여부
+    private boolean hasLarge;   // 라지 사이즈 가능 여부
+    private boolean hasSmall;   // 스몰 사이즈 가능 여부
+    private Long categoryId;    // 카테고리 아이디
 
     public MenuDto(final Product product) {
         this.menuId = product.getId();
@@ -54,4 +62,6 @@ public class MenuDto {
     public void setMenuId(final Long menuId) {
         this.menuId = menuId;
     }
+
+
 }
