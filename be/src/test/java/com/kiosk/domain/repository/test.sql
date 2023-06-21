3b8b8add5dbbc9f994b983298008ea5b2e7b8b8a
YongHwan Kim <yonghwankim.dev@gmail.com>
2023-06-21 21:03:18 +0900

    🐛 fix #20: 빌드 실패 문제 해결 (#22)
    Lombok 라이브러리의 애노테이션 처리 활성화를 위한 문제 해결

---
+++ be/src/test/java/com/kiosk/domain/repository/test.sql
@@ -0,0 +1,85 @@
+INSERT INTO category (category_name) VALUES ('coffee');
+INSERT INTO category (category_name) VALUES ('latte');
+INSERT INTO category (category_name) VALUES ('tea');
+INSERT INTO category (category_name) VALUES ('juice');
+INSERT INTO category (category_name) VALUES ('decaf');
+
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (1, '프리미엄 블렌드 아메리카노', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (1, '콜드부르 딜라이트', 4900, 'asset/MenuImg/asa.png', 0, 0, 1, 1, 0);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (1, '블렌드 아메리카노', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (1, '콜드부르', 4900, 'asset/MenuImg/asa.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (1, '몰라', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (2, '프리미엄 블렌드 아메리카노', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (2, '콜드부르 딜라이트', 4900, 'asset/MenuImg/asa.png', 0, 0, 1, 1, 0);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (2, '블렌드 아메리카노', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (2, '콜드부르', 4900, 'asset/MenuImg/asa.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (2, '몰라', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (3, '프리미엄 블렌드 아메리카노', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (3, '콜드부르 딜라이트', 4900, 'asset/MenuImg/asa.png', 0, 0, 1, 1, 0);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (3, '블렌드 아메리카노', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (3, '콜드부르', 4900, 'asset/MenuImg/asa.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (3, '몰라', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (4, '프리미엄 블렌드 아메리카노', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (4, '콜드부르 딜라이트', 4900, 'asset/MenuImg/asa.png', 0, 0, 1, 1, 0);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (4, '블렌드 아메리카노', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (4, '콜드부르', 4900, 'asset/MenuImg/asa.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (4, '몰라', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (5, '프리미엄 블렌드 아메리카노', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                    product_has_ice, product_has_large, product_has_small)
+VALUES (5, '콜드부르 딜라이트', 4900, 'asset/MenuImg/asa.png', 0, 0, 1, 1, 0);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (5, '블렌드 아메리카노', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (5, '콜드부르', 4900, 'asset/MenuImg/asa.png', 0, 0, 1, 0, 1);
+INSERT INTO product (category_id, product_name, product_price, product_image, product_is_best, product_has_hot,
+                     product_has_ice, product_has_large, product_has_small)
+VALUES (5, '몰라', 4900, 'asset/MenuImg/Americano.png', 0, 0, 1, 0, 1);
