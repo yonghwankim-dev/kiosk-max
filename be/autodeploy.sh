457a135992b10fa74981dff1c5c6c696aa237151
JeeIn Lee <jinny.l.dev@gmail.com>
2023-06-21 20:38:42 +0900

    [BE/Sprint #1]  CI/CD 환경 구성 (#18)
    * 🔧chore #17: Github Actions 워크 플로우 스크립트 작성
    
    * 🔧chore #17: CodeDeploy appspec 스크립트 작성
    
    * 🔧chore #17: CodeDeploy에서 사용할 jar 파일 자동실행 스크립트 작성

---
+++ be/autodeploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+KIOSK_ID=`jps | grep kiosk | awk '{ print $1 }'`
+
+if [ -z $KIOSK_ID ]; then
+echo "동작중인 서버가 없습니다."
+else
+echo "$KIOSK_ID 프로세스를 삭제합니다."
+kill -9 $KIOSK_ID
+fi
+
+echo "서버 시작"
+nohup java -jar ~/app/build/libs/be-0.0.1-SNAPSHOT.jar > ../log.txt 2>&1 &
+echo "배포 완료!"
