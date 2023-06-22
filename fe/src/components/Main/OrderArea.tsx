0af4161dfa3da92cbad769d3c3cd52fa7a21f725
Jisoo Yoo <youzysu@gmail.com>
2023-06-22 15:30:49 +0900

    ✨ feat #33: 주문 영역 타이머, 버튼 UI 생성

--- fe/src/components/Main/OrderArea.tsx
+++ fe/src/components/Main/OrderArea.tsx
@@ -1,4 +1,5 @@
 import { MenuInfo } from 'pages/types';
+import { useEffect, useRef, useState } from 'react';
 import styles from './Main.module.css';
 import MenuItem from './MenuItem';
 
@@ -7,6 +8,19 @@ interface OrderAreaProps {
 }
 
 export default function OrderArea({ orderMenus }: OrderAreaProps) {
+  const [seconds, setSeconds] = useState(30);
+  const intervalRef: { current: null | NodeJS.Timer } = useRef(null);
+
+  useEffect(() => {
+    if (seconds > 0) {
+      intervalRef.current = setInterval(() => {
+        setSeconds(seconds - 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalRef.current!);
+  }, [seconds]);
+
   // TODO: dispatch deleteOrder action 넘기기
   return (
     <div className={styles.orderArea}>
@@ -22,12 +36,18 @@ export default function OrderArea({ orderMenus }: OrderAreaProps) {
                 menuImg={menu.imgUrl}
                 menuPrice={menu.price}
               />
-              <button onClick={() => {}}></button>
+              <button className={styles.menuCancelButton} onClick={() => {}}>
+                X
+              </button>
             </div>
           );
         })}
       </div>
-      <div></div>
+      <div className={styles.buttons}>
+        <button className={styles.allCancelButton}>전체취소</button>
+        <button className={styles.orderButton}>결제하기</button>
+        <span className={styles.timer}>결제하기 버튼을 누르지 않으면 {seconds}초 뒤에 메뉴가 전체 취소돼요!</span>
+      </div>
     </div>
   );
 }
