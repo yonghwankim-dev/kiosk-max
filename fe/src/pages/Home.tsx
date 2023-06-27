f2a39ea20268e01fe85d16af18c7a3fb1dfaea7f
Jisoo Yoo <youzysu@gmail.com>
2023-06-27 17:49:13 +0900

    ✨ feat #48: 클라이언트 라우팅 처리

--- fe/src/pages/Home.tsx
+++ fe/src/pages/Home.tsx
@@ -1,18 +1,37 @@
+import { fetchMenus } from 'api';
 import Main from 'components/Main';
 import Cart from 'components/Main/Cart';
 import CategoryNavbar from 'components/Navbar';
 import menuOrderReducer from 'menuOrderReducer';
-import { useMemo, useReducer, useRef, useState } from 'react';
+import { useEffect, useMemo, useReducer, useRef, useState } from 'react';
 import { formatAllCategories, formatProducts } from 'utils';
-import styles from './MainPage.module.css';
+import styles from './Home.module.css';
 import { CategoryInfo, MenuOrder } from './types';
 
 interface MainPageProps {
-  products: CategoryInfo[];
+  navigate: (path: string) => void;
 }
 
-export default function MainPage({ products }: MainPageProps) {
-  const [selectedCategoryId, setSelectedCategoryId] = useState<number>(products[0].categoryId);
+export default function MainPage({ navigate }: MainPageProps) {
+  const [products, setProducts] = useState<CategoryInfo[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const getMenus = async () => {
+    const menuData = await fetchMenus();
+
+    if (!menuData) {
+      return;
+    }
+
+    setProducts(menuData);
+    setSelectedCategoryId(menuData[0].categoryId);
+  };
+
+  useEffect(() => {
+    getMenus();
+  }, []);
+
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number>(0);
   const [orderList, dispatch] = useReducer(menuOrderReducer, []);
   const mainPageRef = useRef<HTMLDivElement>(null);
 
@@ -25,7 +44,7 @@ export default function MainPage({ products }: MainPageProps) {
   );
   const formattedMenuData = useMemo(() => formatAllCategories(products), [products]);
   const formattedMenus = useMemo(() => formatProducts(products), [products]);
-  const currentMenus = formattedMenuData[selectedCategoryId].menus;
+  const currentMenus = formattedMenuData[selectedCategoryId];
   const isOrderListEmpty = orderList.length === 0;
 
   const handleCategoryClick = (clickCategoryId: number) => setSelectedCategoryId(clickCategoryId);
@@ -34,15 +53,18 @@ export default function MainPage({ products }: MainPageProps) {
   const handleRemoveAllOrders = () => dispatch({ type: 'RESET' });
 
   return (
-    <div ref={mainPageRef} className={styles.mainPage}>
-      <CategoryNavbar
-        selectedCategoryId={selectedCategoryId}
-        categories={categoryNavbarInfo}
-        handleCategoryClick={handleCategoryClick}
-      />
-      <Main handleAddOrder={handleAddOrder} menus={currentMenus} />
+    <div ref={mainPageRef} className={styles.home}>
+      {selectedCategoryId && (
+        <CategoryNavbar
+          selectedCategoryId={selectedCategoryId}
+          categories={categoryNavbarInfo}
+          handleCategoryClick={handleCategoryClick}
+        />
+      )}
+      {currentMenus && <Main handleAddOrder={handleAddOrder} menus={currentMenus.menus} />}
       {!isOrderListEmpty && (
         <Cart
+          // navigate={navigate}
           mainPageRef={mainPageRef}
           menus={formattedMenus}
           orderList={orderList}
