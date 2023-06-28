08286b894d2df967747c42967e993f33458f416d
Jisoo Yoo <youzysu@gmail.com>
2023-06-29 06:44:44 +0900

    🐛 fix: 영수증 API 변경 인터페이스에 맞춰 Receipt 컴포넌트 수정

--- fe/src/components/Receipt/Receipt.tsx
+++ fe/src/components/Receipt/Receipt.tsx
@@ -1,10 +1,10 @@
-import styles from './Receipt.module.css';
 import { OrderSuccessInfo } from '../../pages/types';
 import { calculateTotalAmount } from '../../utils';
+import styles from './Receipt.module.css';
 
 export default function Receipt({ data }: { data: OrderSuccessInfo }) {
-  const paymentMethod = data.paymentMethod === 'cash' ? '현금' : '카드';
-  const isCashPayment = data.paymentMethod === 'cash';
+  const paymentMethod = data.payment.method === 'cash' ? '현금' : '카드';
+  const isCashPayment = data.payment.method === 'cash';
   const calculatedAmount = calculateTotalAmount(data);
   const keys = Object.keys(calculatedAmount);
   const mappedArray = keys.map(key => ({
@@ -15,7 +15,7 @@ export default function Receipt({ data }: { data: OrderSuccessInfo }) {
   return (
     <div className={styles.receiptWrap}>
       <h1 className={styles.orderNumber}>{`주문번호 ${data.orderNumber}`}</h1>
-      <span className={styles.orderTime}>{`${data.orderDatetime}`}</span>
+      <span className={styles.orderTime}>{`${data.payment.orderDatetime}`}</span>
       <div className={styles.orderLists}>
         {mappedArray.map((list: OrderListProps, index: number) => {
           return <OrderList key={index} list={list} />;
@@ -23,9 +23,9 @@ export default function Receipt({ data }: { data: OrderSuccessInfo }) {
       </div>
       <div className={styles.paymentInfo}>
         <span>{`결제방식: ${paymentMethod}`}</span>
-        {isCashPayment && <span>{`투입금액: ${data.receivedPrice}`}</span>}
-        <span>{`총 결제금액: ${data.totalPrice}`}</span>
-        {isCashPayment && <span>{`거스름돈: ${data.remainedPrice}`}</span>}
+        {isCashPayment && <span>{`투입금액: ${data.payment.receivedPrice}`}</span>}
+        <span>{`총 결제금액: ${data.payment.totalPrice}`}</span>
+        {isCashPayment && <span>{`거스름돈: ${data.payment.remainedPrice}`}</span>}
       </div>
     </div>
   );
