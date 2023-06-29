cb827b5e29acf4b4f172dc0102909248cefe317c
Jisoo Yoo <youzysu@gmail.com>
2023-06-29 13:12:13 +0900

    ✨ feat #64: 카트에서 메뉴 삭제 시 동일한 사이즈만 삭제

--- fe/src/menuOrderReducer.ts
+++ fe/src/menuOrderReducer.ts
@@ -2,7 +2,7 @@ import { ProductOrder } from 'pages/types';
 
 export type ActionMap = {
   ADD_ORDER: { newOrder: ProductOrder };
-  REMOVE_ORDER: { productId: number };
+  REMOVE_ORDER: { productId: number; size: string };
   RESET: {};
 };
 
@@ -16,10 +16,17 @@ export type MenuOrderAction = {
 export default function menuOrderReducer(initialOrders: ProductOrder[], action: MenuOrderAction): ProductOrder[] {
   switch (action.type) {
     case 'ADD_ORDER': {
-      return [...initialOrders, action.payload!.newOrder];
+      if (!action.payload) {
+        throw new Error('REMOVE_ORDER action must have payload');
+      }
+      return [...initialOrders, action.payload.newOrder];
     }
     case 'REMOVE_ORDER': {
-      return initialOrders.filter(order => order.productId !== action.payload!.productId);
+      if (!action.payload) {
+        throw new Error('REMOVE_ORDER action must have payload');
+      }
+      const { productId, size } = action.payload;
+      return initialOrders.filter(order => order.productId !== productId || order.size !== size);
     }
     case 'RESET': {
       return [];
